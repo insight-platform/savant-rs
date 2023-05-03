c77a1506279e8557d5b58676c21cb0390f426a91
bwsw <bitworks@bw-sw.com>
2023-05-03 23:49:10 +0400

    added rkyv support

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod primitives;
 use pyo3::prelude::*;
 
 #[pymodule]
-fn trackreg(_py: Python, m: &PyModule) -> PyResult<()> {
+fn savant_primitives(_py: Python, m: &PyModule) -> PyResult<()> {
     let _ = env_logger::try_init();
     m.add_class::<primitives::Point>()?;
     m.add_class::<primitives::PolygonalArea>()?;
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,10 +1,13 @@
 use geo::{Contains, LineString};
 use pyo3::prelude::*;
+use rkyv::{with::Skip, Archive, Deserialize, Serialize};
 
 #[pyclass]
-#[derive(Debug, Clone)]
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq, Clone)]
+#[archive(check_bytes)]
 pub struct Point {
-    point: geo::Point,
+    x: f64,
+    y: f64,
 }
 
 #[pymethods]
@@ -22,16 +25,17 @@ impl Point {
 
     #[new]
     fn new(x: f64, y: f64) -> Self {
-        Self {
-            point: geo::Point::from((x, y)),
-        }
+        Self { x, y }
     }
 }
 
 #[pyclass]
-#[derive(Debug)]
+#[derive(Archive, Deserialize, Serialize, Debug, Clone, Default)]
+#[archive(check_bytes)]
 pub struct PolygonalArea {
-    pub vertices: geo::Polygon,
+    pub vertices: Vec<Point>,
+    #[with(Skip)]
+    polygon: Option<geo::Polygon>,
 }
 
 #[pymethods]
@@ -49,22 +53,43 @@ impl PolygonalArea {
 
     #[new]
     pub fn new(vertices: Vec<Point>) -> Self {
-        let exterior = vertices
-            .iter()
-            .map(|p| geo::Point::from((p.point.x(), p.point.y())))
-            .collect::<Vec<geo::Point>>();
-
-        Self {
-            vertices: geo::Polygon::new(LineString::from(exterior), vec![]),
-        }
+        let polygon = Some(Self::gen_polygon(&vertices));
+        Self { polygon, vertices }
     }
 
-    pub fn contains(&self, p: &Point) -> bool {
-        self.vertices.contains(&p.point)
+    pub fn contains(&mut self, p: &Point) -> bool {
+        self.gen_poly();
+        self.polygon
+            .as_ref()
+            .unwrap()
+            .contains(&geo::Point::from((p.x, p.y)))
     }
 
-    pub fn contains_many(&self, points: Vec<Point>) -> Vec<bool> {
+    pub fn contains_many(&mut self, points: Vec<Point>) -> Vec<bool> {
+        self.gen_poly();
         points.iter().map(|p| self.contains(p)).collect()
+    }
+}
+
+impl PolygonalArea {
+    fn gen_polygon(vertices: &Vec<Point>) -> geo::Polygon {
+        geo::Polygon::new(
+            LineString::from(
+                vertices
+                    .iter()
+                    .map(|p| geo::Point::from((p.x, p.y)))
+                    .collect::<Vec<geo::Point>>(),
+            ),
+            vec![],
+        )
+    }
+
+    fn gen_poly(&mut self) {
+        let p = self
+            .polygon
+            .take()
+            .unwrap_or_else(|| Self::gen_polygon(&self.vertices));
+        self.polygon.replace(p);
     }
 }
 
@@ -77,7 +102,7 @@ mod tests {
         let p1 = Point::new(0.0, 0.0);
         let p2 = Point::new(0.99, 0.0);
         let p3 = Point::new(1.0, 0.0);
-        let area = PolygonalArea::new(vec![
+        let mut area = PolygonalArea::new(vec![
             Point::new(-1.0, 1.0),
             Point::new(1.0, 1.0),
             Point::new(1.0, -1.0),
@@ -92,5 +117,25 @@ mod tests {
             area.contains_many(vec![p1, p2, p3]),
             vec![true, true, false]
         );
+    }
+
+    #[test]
+    fn archive() {
+        let area = PolygonalArea::new(vec![
+            Point::new(-1.0, 1.0),
+            Point::new(1.0, 1.0),
+            Point::new(1.0, -1.0),
+            Point::new(-1.0, -1.0),
+        ]);
+        let bytes = rkyv::to_bytes::<_, 256>(&area).unwrap();
+        let area2 = rkyv::from_bytes::<PolygonalArea>(&bytes[..]);
+        assert!(area2.is_ok());
+        assert_eq!(area2.unwrap().vertices, area.vertices);
+
+        let area_err = rkyv::from_bytes::<PolygonalArea>(vec![].as_slice());
+        assert!(area_err.is_err());
+
+        let area_err = rkyv::from_bytes::<PolygonalArea>(vec![1, 2, 3].as_slice());
+        assert!(area_err.is_err());
     }
 }
