9c4fe0dcbddbb3420f9a9461913fe0e88e541da0
bwsw <bitworks@bw-sw.com>
2023-05-12 12:11:32 +0400

    implemented example for crossing

---
+++ python/primitives/polygon_match.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+from savant_rs.primitives import PolygonalArea, Point, Segment
+from timeit import default_timer as timer
+
+area = PolygonalArea([Point(-1, 1), Point(1, 1), Point(1, -1), Point(-1, -1)], ["up", None, "down", None])
+
+crosses_13 = Segment(Point(-2, 0), Point(2, 0))
+crosses_02 = Segment(Point(0, -2), Point(0, 2))
+leaves_vertex = Segment(Point(0, 0), Point(2, 2))
+crosses_vertices = Segment(Point(-2, -2), Point(2, 2))
+crosses_whole_edge = Segment(Point(-2, 1), Point(2, 1))
+enters_vertex = Segment(Point(2, 2), Point(0, 0))
+outside = Segment(Point(-2, 2), Point(2, 2))
+inside = Segment(Point(-0.5, -0.5), Point(0.5, 0.5))
+
+l = [crosses_13, crosses_02, leaves_vertex, crosses_vertices, crosses_whole_edge, enters_vertex, outside, inside]
+res = area.crossed_by_segments(l)
+pprint(list(zip(l, res)))
