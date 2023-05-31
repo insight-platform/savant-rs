ecca4782ae062246d448b952519a90a4f7306f9f
bwsw <bitworks@bw-sw.com>
2023-05-31 19:04:45 +0400

    fixed cbindgen

--- savant/build.rs
+++ savant/build.rs
@@ -4,10 +4,8 @@ fn main() {
     pyo3_build_config::add_extension_module_link_args();
 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let config = cbindgen::Config {
-        language: cbindgen::Language::C,
-        ..Default::default()
-    };
+    let mut config = cbindgen::Config::default();
+    config.language = cbindgen::Language::C;
     cbindgen::generate_with_config(crate_dir, config)
         .unwrap()
         .write_to_file("capi/savant_rs.h");
