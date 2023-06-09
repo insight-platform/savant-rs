b8bd06824f15941ee6903d401b5f6d4a5311666e
bwsw <bitworks@bw-sw.com>
2023-06-09 20:55:16 +0400

    install protoc from github

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -e
 
-apt-get update
-apt-get install -y protobuf-compiler
-pip install maturin==0.15
+ARCH=$(uname -m)
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+
+mkdir -p bin && cd bin
+
+if [ "$ARCH" = "x86_64" ]; then
+    echo "Installing x86_64 dependencies"
+    curl -LO $PB_REL/download/v23.2/protoc-23.2-linux-x86_64.zip
+    unzip protoc-23.2-linux-x86_64.zip
+elif [ "$ARCH" = "aarch64" ]; then
+    echo "Installing aarch64 dependencies"
+    curl -LO $PB_REL/download/v23.2/protoc-23.2-linux-aarch_64.zip
+    unzip protoc-23.2-linux-aarch_64.zip
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+export PATH=$PATH:$PWD/bin
