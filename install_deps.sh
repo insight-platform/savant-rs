070472ce02398ce490dcfb46eaacb250e8b6db1a
bwsw <bitworks@bw-sw.com>
2023-06-09 21:51:57 +0400

    test build

--- install_deps.sh
+++ install_deps.sh
@@ -1,22 +1,4 @@
 #!/bin/sh -e
-#
-#ARCH=$(uname -m)
-#PB_REL="https://github.com/protocolbuffers/protobuf/releases"
-#cd ..
-#
-#DIR='/home/runner/work/savant-rs/bin/protoc'
-#
-#if [ "$ARCH" = "x86_64" ]; then
-#    echo "Installing x86_64 dependencies"
-#    curl -LO $PB_REL/download/v23.2/protoc-23.2-linux-x86_64.zip
-#    unzip protoc-23.2-linux-x86_64.zip
-#elif [ "$ARCH" = "aarch64" ]; then
-#    echo "Installing aarch64 dependencies"
-#    curl -LO $PB_REL/download/v23.2/protoc-23.2-linux-aarch_64.zip
-#    unzip protoc-23.2-linux-aarch_64.zip
-#else
-#    echo "Unsupported architecture: $ARCH"
-#    exit 1
-#fi
-#
-#export PATH=$PATH:$PWD/bin
+
+apt-get update
+apt-get install protobuf-compiler -y
