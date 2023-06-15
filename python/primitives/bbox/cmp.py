b2e738b349ab54970e42ee67d549f79e92a4558f
bwsw <bitworks@bw-sw.com>
2023-06-15 23:24:42 +0400

    added comparison ops

---
+++ python/primitives/bbox/cmp.py
@@ -0,0 +1,36 @@
+from savant_rs.primitives.geometry import BBox, RBBox
+
+box1 = BBox(50, 50, 50, 50)
+box2 = BBox(50, 50, 50, 50)
+box3 = BBox(50, 50, 50, 60)
+
+assert box1 == box2
+assert box1.eq(box2)
+assert box1.almost_eq(box2, 0.1)
+
+assert box1 != box3
+assert not box1.eq(box3)
+
+box1 = RBBox(50, 50, 50, 50, 30)
+box2 = RBBox(50, 50, 50, 50, 30)
+box3 = RBBox(50, 50, 50, 50, 30.001)
+
+assert box1 == box2
+assert box1.eq(box2)
+assert box1.almost_eq(box2, 0.1)
+
+assert box1 != box3
+assert box1.almost_eq(box3, 0.1)
+
+iou = box1.iou(box2)
+assert iou == 1.0
+
+iou = box1.iou(box3)
+assert iou > 0.9
+
+for f in [lambda: box1 > box2, lambda: box1 < box2, lambda: box1 >= box2, lambda: box1 <= box2]:
+    try:
+        f()
+        assert False
+    except NotImplementedError:
+        pass
