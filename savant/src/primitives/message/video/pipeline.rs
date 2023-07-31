baeb6f9f2852329f140f746d9e43028b630543f9
bwsw <bitworks@bw-sw.com>
2023-07-31 18:57:52 +0400

    implemented pipeline tests

--- savant/src/primitives/message/video/pipeline.rs
+++ savant/src/primitives/message/video/pipeline.rs
@@ -3,13 +3,21 @@ use hashbrown::HashMap;
 use pyo3::prelude::*;
 
 #[derive(Clone, Debug)]
-pub struct PipelineStage {
+pub struct VideoPipelineStage {
     payload: HashMap<i64, PipelinePayload>,
+}
+
+impl Default for VideoPipelineStage {
+    fn default() -> Self {
+        Self {
+            payload: HashMap::new(),
+        }
+    }
 }
 
 #[pyclass]
 #[derive(Clone, Debug, PartialEq)]
-pub enum PipelineStagePayloadType {
+pub enum VideoPipelineStagePayloadType {
     Frame,
     Batch,
 }
@@ -23,34 +31,34 @@ pub enum PipelinePayload {
 #[derive(Clone, Debug, Default)]
 pub struct VideoPipeline {
     pub id_counter: i64,
-    pub stages: HashMap<String, PipelineStage>,
-    pub stage_types: HashMap<String, PipelineStagePayloadType>,
+    pub stages: HashMap<String, VideoPipelineStage>,
+    pub stage_types: HashMap<String, VideoPipelineStagePayloadType>,
 }
 
 impl VideoPipeline {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
     pub fn add_stage(
         &mut self,
         name: &str,
-        stage: PipelineStage,
-        stage_type: PipelineStagePayloadType,
-    ) {
-        self.stages.insert(name.to_owned(), stage);
+        stage_type: VideoPipelineStagePayloadType,
+    ) -> anyhow::Result<()> {
+        if self.stages.contains_key(name) {
+            anyhow::bail!("Stage already exists")
+        }
+        self.stages
+            .insert(name.to_owned(), VideoPipelineStage::default());
         self.stage_types.insert(name.to_owned(), stage_type);
+        Ok(())
     }
 
-    pub fn get_stage(&self, name: &str) -> Option<&PipelineStage> {
+    pub fn get_stage(&self, name: &str) -> Option<&VideoPipelineStage> {
         self.stages.get(name)
     }
 
-    pub fn get_stage_mut(&mut self, name: &str) -> Option<&mut PipelineStage> {
+    pub fn get_stage_mut(&mut self, name: &str) -> Option<&mut VideoPipelineStage> {
         self.stages.get_mut(name)
     }
 
-    pub fn get_stage_type(&self, name: &str) -> Option<&PipelineStagePayloadType> {
+    pub fn get_stage_type(&self, name: &str) -> Option<&VideoPipelineStagePayloadType> {
         self.stage_types.get(name)
     }
 
@@ -104,7 +112,7 @@ impl VideoPipeline {
     pub fn add_frame(&mut self, stage: &str, frame: VideoFrameProxy) -> anyhow::Result<i64> {
         if matches!(
             self.get_stage_type(stage),
-            Some(PipelineStagePayloadType::Batch)
+            Some(VideoPipelineStagePayloadType::Batch)
         ) {
             anyhow::bail!("Stage does not accept batched frames")
         }
@@ -124,7 +132,7 @@ impl VideoPipeline {
     pub fn add_batch(&mut self, stage: &str, batch: VideoFrameBatch) -> anyhow::Result<i64> {
         if matches!(
             self.get_stage_type(stage),
-            Some(PipelineStagePayloadType::Frame)
+            Some(VideoPipelineStagePayloadType::Frame)
         ) {
             anyhow::bail!("Stage does not accept independent frames")
         }
@@ -142,7 +150,9 @@ impl VideoPipeline {
 
     pub fn del(&mut self, stage: &str, id: i64) -> anyhow::Result<()> {
         if let Some(stage) = self.get_stage_mut(stage) {
-            stage.payload.remove(&id);
+            if stage.payload.remove(&id).is_none() {
+                anyhow::bail!("Object not found in stage")
+            }
         } else {
             anyhow::bail!("Stage not found")
         }
@@ -283,10 +293,10 @@ impl VideoPipeline {
     ) -> anyhow::Result<i64> {
         if matches!(
             self.get_stage_type(source_stage),
-            Some(PipelineStagePayloadType::Batch)
+            Some(VideoPipelineStagePayloadType::Batch)
         ) || matches!(
             self.get_stage_type(dest_stage),
-            Some(PipelineStagePayloadType::Frame)
+            Some(VideoPipelineStagePayloadType::Frame)
         ) {
             anyhow::bail!("Source stage must contain independent frames and destination stage must contain batched frames")
         }
@@ -339,10 +349,10 @@ impl VideoPipeline {
     ) -> anyhow::Result<()> {
         if matches!(
             self.get_stage_type(source_stage),
-            Some(PipelineStagePayloadType::Frame)
+            Some(VideoPipelineStagePayloadType::Frame)
         ) || matches!(
             self.get_stage_type(dest_stage),
-            Some(PipelineStagePayloadType::Batch)
+            Some(VideoPipelineStagePayloadType::Batch)
         ) {
             anyhow::bail!("Source stage must contain batched frames and destination stage must contain independent frames")
         }
@@ -394,4 +404,186 @@ impl VideoPipeline {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::primitives::attribute::AttributeMethods;
+    use crate::primitives::message::video::pipeline::{
+        VideoPipeline, VideoPipelineStagePayloadType,
+    };
+    use crate::primitives::{AttributeBuilder, AttributeValue, VideoFrameBatch, VideoFrameUpdate};
+    use crate::test::utils::gen_frame;
+
+    fn create_pipeline() -> anyhow::Result<VideoPipeline> {
+        let mut pipeline = VideoPipeline::default();
+        pipeline.add_stage("input", VideoPipelineStagePayloadType::Frame)?;
+        pipeline.add_stage("proc1", VideoPipelineStagePayloadType::Batch)?;
+        pipeline.add_stage("proc2", VideoPipelineStagePayloadType::Batch)?;
+        pipeline.add_stage("output", VideoPipelineStagePayloadType::Frame)?;
+        Ok(pipeline)
+    }
+
+    #[test]
+    fn test_new_pipeline() -> anyhow::Result<()> {
+        let pipeline = create_pipeline()?;
+        assert_eq!(pipeline.id_counter, 0);
+        assert_eq!(pipeline.stages.len(), 4);
+        assert_eq!(pipeline.stage_types.len(), 4);
+        Ok(())
+    }
+
+    #[test]
+    fn test_add_duplicate_stage() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        assert!(pipeline
+            .add_stage("input", VideoPipelineStagePayloadType::Frame)
+            .is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_stage() -> anyhow::Result<()> {
+        let pipeline = create_pipeline()?;
+        assert!(pipeline.get_stage("input").is_some());
+        assert!(pipeline.get_stage("proc1").is_some());
+        assert!(pipeline.get_stage("proc2").is_some());
+        assert!(pipeline.get_stage("output").is_some());
+        assert!(pipeline.get_stage("unknown").is_none());
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_stage_mut() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        assert!(pipeline.get_stage_mut("input").is_some());
+        assert!(pipeline.get_stage_mut("proc1").is_some());
+        assert!(pipeline.get_stage_mut("proc2").is_some());
+        assert!(pipeline.get_stage_mut("output").is_some());
+        assert!(pipeline.get_stage_mut("unknown").is_none());
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_stage_type() -> anyhow::Result<()> {
+        let pipeline = create_pipeline()?;
+        assert!(matches!(
+            pipeline.get_stage_type("input"),
+            Some(VideoPipelineStagePayloadType::Frame)
+        ));
+        assert!(matches!(
+            pipeline.get_stage_type("proc1"),
+            Some(VideoPipelineStagePayloadType::Batch)
+        ));
+        Ok(())
+    }
+
+    #[test]
+    fn test_add_del_frame() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_frame("input", gen_frame())?;
+        assert!(pipeline.add_frame("proc1", gen_frame()).is_err());
+
+        assert!(pipeline.del("proc1", id).is_err());
+        pipeline.del("input", id)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_add_del_batch() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_batch("proc1", VideoFrameBatch::new())?;
+        assert!(pipeline.add_batch("input", VideoFrameBatch::new()).is_err());
+
+        assert!(pipeline.del("input", id).is_err());
+        pipeline.del("proc1", id)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_frame_to_batch() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_frame("input", gen_frame())?;
+        let batch_id = pipeline.move_and_pack_frames("input", "proc1", vec![id])?;
+
+        assert!(pipeline.get_independent_frame("input", id).is_err());
+
+        assert!(pipeline.get_batch("proc1", batch_id).is_ok());
+        assert!(pipeline.get_batched_frame("proc1", batch_id, id).is_ok());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_batch_to_frame() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_frame("input", gen_frame())?;
+        let batch_id = pipeline.move_and_pack_frames("input", "proc2", vec![id])?;
+        pipeline.move_and_unpack_batch("proc2", "output", batch_id)?;
+        let _frame = pipeline.get_independent_frame("output", id)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_batch_to_batch() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_frame("input", gen_frame())?;
+        let batch_id = pipeline.move_and_pack_frames("input", "proc1", vec![id])?;
+        pipeline.move_as_is("proc1", "proc2", vec![batch_id])?;
+        let _batch = pipeline.get_batch("proc2", batch_id)?;
+        let _frame = pipeline.get_batched_frame("proc2", batch_id, id)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_frame_to_frame() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_frame("input", gen_frame())?;
+        pipeline.move_as_is("input", "output", vec![id])?;
+        let _frame = pipeline.get_independent_frame("output", id)?;
+        Ok(())
+    }
+
+    fn get_update() -> VideoFrameUpdate {
+        let mut update = VideoFrameUpdate::new();
+        update.add_attribute(
+            &AttributeBuilder::default()
+                .namespace("update".into())
+                .name("attribute".into())
+                .hint(None)
+                .hint(Some("test".into()))
+                .values(vec![AttributeValue::string("1".into(), None)])
+                .build()
+                .unwrap(),
+        );
+        update
+    }
+
+    #[test]
+    fn test_frame_update() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_frame("input", gen_frame())?;
+        let update = get_update();
+        pipeline.add_frame_update("input", id, update)?;
+        pipeline.apply_updates("input", id)?;
+        let frame = pipeline.get_independent_frame("input", id)?;
+        frame
+            .get_attribute("update".to_string(), "attribute".to_string())
+            .unwrap();
+        Ok(())
+    }
+
+    #[test]
+    fn test_batch_update() -> anyhow::Result<()> {
+        let mut pipeline = create_pipeline()?;
+        let id = pipeline.add_frame("input", gen_frame())?;
+        let batch_id = pipeline.move_and_pack_frames("input", "proc1", vec![id])?;
+        let update = get_update();
+        pipeline.add_batched_frame_update("proc1", batch_id, id, update)?;
+        pipeline.apply_updates("proc1", batch_id)?;
+        let frame = pipeline.get_batched_frame("proc1", batch_id, id)?;
+        frame
+            .get_attribute("update".to_string(), "attribute".to_string())
+            .unwrap();
+        Ok(())
+    }
+}
