9eb5cefbc814bcd8753df1e335397ceea42b3c5e
bwsw <bitworks@bw-sw.com>
2023-08-16 14:48:37 +0400

    ci build

--- savant_capi/build/build.sh
+++ savant_capi/build/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-ARCH=$1
-
 set -e
 
 rustup target add $ARCH-unknown-linux-gnu
+rustup toolchain install stable-aarch64-unknown-linux-gnu
 
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
@@ -12,4 +11,10 @@ unzip protoc-3.15.8-linux-x86_64.zip -d $HOME/.local
 export PATH="$PATH:$HOME/.local/bin"
 
 cd savant_capi
-cargo build --target $ARCH-unknown-linux-gnu # --release
+cargo build --target x86_64-unknown-linux-gnu # --release
+
+ENV CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
+    CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc \
+    CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
+
+cargo build --target aarch64-unknown-linux-gnu
