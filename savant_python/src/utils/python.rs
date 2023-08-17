1830c84339b13fca034dadf99ff154fe2f75a9b5
bwsw <bitworks@bw-sw.com>
2023-08-17 19:52:45 +0400

    fixed build

--- savant_python/src/utils/python.rs
+++ savant_python/src/utils/python.rs
@@ -1,4 +1,21 @@
 #[macro_export]
+macro_rules! function {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+
+        // Find and cut the rest of the path
+        match &name[..name.len() - 3].rfind(':') {
+            Some(pos) => &name[pos + 1..name.len() - 3],
+            None => &name[..name.len() - 3],
+        }
+    }};
+}
+
+#[macro_export]
 macro_rules! with_gil {
     ($expression:expr) => {{
         let start = std::time::Instant::now();
@@ -9,7 +26,7 @@ macro_rules! with_gil {
             "savant::gil_management::with_gil".to_string(),
             format!(
                 "Holding GIL ({}, {}, {})",
-                savant_core::function!(),
+                $crate::function!(),
                 file!(),
                 line!()
             ),
@@ -37,7 +54,7 @@ macro_rules! with_trace {
             "savant::trace".to_string(),
             format!(
                 "Tracing ({}, {}, {})",
-                savant_core::function!(),
+                $crate::function!(),
                 file!(),
                 line!()
             ),
@@ -77,7 +94,7 @@ macro_rules! release_gil {
                 format!(
                     "{} GIL-free operation ({}, {}, {})",
                     if gf > 10000 { "👌" } else { "💀" },
-                    savant_core::function!(),
+                    $crate::function!(),
                     file!(),
                     line!()
                 ),
