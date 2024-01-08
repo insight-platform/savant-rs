b1ff640369f3140e27b6f4a1eafddae637230540
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2024-01-08 10:35:47 +0100

    implemented for start/shutdown tests

--- savant_core/src/transport/zeromq/nonblocking_reader.rs
+++ savant_core/src/transport/zeromq/nonblocking_reader.rs
@@ -7,6 +7,7 @@ pub struct NonblockingReader {
     config: ReaderConfig,
     thread: Option<std::thread::JoinHandle<()>>,
     receiver: Option<Receiver<anyhow::Result<ReaderResult>>>,
+    is_started: OnceLock<()>,
     is_shutdown: Arc<OnceLock<()>>,
 }
 
@@ -16,11 +17,19 @@ impl NonblockingReader {
             config: config.clone(),
             thread: None,
             receiver: None,
+            is_started: OnceLock::new(),
             is_shutdown: Arc::new(OnceLock::new()),
         })
     }
 
     pub fn start(&mut self) -> anyhow::Result<()> {
+        if self.is_shutdown() {
+            anyhow::bail!("Reader is shutdown.");
+        }
+        if self.is_started() {
+            anyhow::bail!("Reader is already started.");
+        }
+        _ = self.is_started.set(());
         let (sender, receiver) = crossbeam_channel::unbounded();
         let reader = SyncReader::new(&self.config)?;
         let is_shutdown = self.is_shutdown.clone();
@@ -40,7 +49,17 @@ impl NonblockingReader {
         self.is_shutdown.get().is_some()
     }
 
+    pub fn is_started(&self) -> bool {
+        self.is_started.get().is_some()
+    }
+
     pub fn shutdown(&mut self) -> anyhow::Result<()> {
+        if self.is_shutdown() {
+            anyhow::bail!("Reader is shutdown.");
+        }
+        if !self.is_started() {
+            anyhow::bail!("Reader is not started.");
+        }
         if let Some(thread) = self.thread.take() {
             _ = self.is_shutdown.set(());
             thread
@@ -53,6 +72,9 @@ impl NonblockingReader {
     }
 
     pub fn receive(&self) -> anyhow::Result<ReaderResult> {
+        if !self.is_started() {
+            anyhow::bail!("Reader is not started.");
+        }
         if self.is_shutdown() {
             anyhow::bail!("Reader is shutdown.");
         }
@@ -66,8 +88,11 @@ impl NonblockingReader {
     }
 
     pub fn try_receive(&self) -> Option<anyhow::Result<ReaderResult>> {
+        if !self.is_started() {
+            return Some(Err(anyhow::anyhow!("Reader is not started.")));
+        }
         if self.is_shutdown() {
-            return None;
+            return Some(Err(anyhow::anyhow!("Reader is shutdown.")));
         }
         if let Some(receiver) = &self.receiver {
             match receiver.try_recv() {
@@ -125,6 +150,64 @@ mod tests {
         assert!(elapsed < 100);
         assert!(recv.is_none());
         reader.shutdown()?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_double_start() -> anyhow::Result<()> {
+        let conf = ReaderConfig::new()
+            .url("router+bind:ipc:///tmp/test/nonblocking-reader-double-start")?
+            .with_topic_prefix_spec(TopicPrefixSpec::SourceId("topic".into()))?
+            .with_receive_timeout(100)?
+            .build()?;
+        let mut reader = super::NonblockingReader::new(&conf)?;
+        reader.start()?;
+        let res = reader.start();
+        assert!(res.is_err());
+        reader.shutdown()?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_double_shutdown() -> anyhow::Result<()> {
+        let conf = ReaderConfig::new()
+            .url("router+bind:ipc:///tmp/test/nonblocking-reader-double-shutdown")?
+            .with_topic_prefix_spec(TopicPrefixSpec::SourceId("topic".into()))?
+            .with_receive_timeout(100)?
+            .build()?;
+        let mut reader = super::NonblockingReader::new(&conf)?;
+        reader.start()?;
+        reader.shutdown()?;
+        let res = reader.shutdown();
+        assert!(res.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_shutdown_without_start() -> anyhow::Result<()> {
+        let conf = ReaderConfig::new()
+            .url("router+bind:ipc:///tmp/test/nonblocking-reader-shutdown-without-start")?
+            .with_topic_prefix_spec(TopicPrefixSpec::SourceId("topic".into()))?
+            .with_receive_timeout(100)?
+            .build()?;
+        let mut reader = super::NonblockingReader::new(&conf)?;
+        let res = reader.shutdown();
+        assert!(res.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_recv_without_start() -> anyhow::Result<()> {
+        let conf = ReaderConfig::new()
+            .url("router+bind:ipc:///tmp/test/nonblocking-reader-recv-without-start")?
+            .with_topic_prefix_spec(TopicPrefixSpec::SourceId("topic".into()))?
+            .with_receive_timeout(100)?
+            .build()?;
+        let reader = super::NonblockingReader::new(&conf)?;
+        let res = reader.receive();
+        assert!(res.is_err());
+        let res = reader.try_receive().unwrap();
+        assert!(res.is_err());
         Ok(())
     }
 }
