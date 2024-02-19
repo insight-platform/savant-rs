76343eabfa8c77e3729cac92649a244fc9a57e94
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2024-02-19 08:36:25 +0100

    project decomposition

--- savant_core_py/src/primitives/object.rs
+++ savant_core_py/src/primitives/object.rs
@@ -500,7 +500,6 @@ impl BorrowedVideoObject {
 
     #[getter]
     pub fn get_track_id(&self) -> Option<i64> {
-        warn!("get_track_id is deprecated, use track_id instead");
         self.0.get_track_id()
     }
 
