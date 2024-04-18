b2bd381ebc9ce0e92667405be7acfc61764998d0
Ivan Kud <bwsw@users.noreply.github.com>
2024-04-18 09:16:24 +0200

    improved log messages (#139)

--- savant_core/src/utils.rs
+++ savant_core/src/utils.rs
@@ -1,2 +1,10 @@
 pub mod default_once;
 pub mod iter;
+use std::fmt::Write;
+
+pub fn bytes_to_hex_string(bytes: &[u8]) -> String {
+    bytes.iter().fold(String::new(), |mut output, b| {
+        let _ = write!(output, "{b:02X}");
+        output
+    })
+}
