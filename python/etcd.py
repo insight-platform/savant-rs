673fb6e2b711730b2db62272a24d4668fb635553
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2024-06-17 15:24:44 +0200

    added etcd sample test

---
+++ python/etcd.py
@@ -0,0 +1,34 @@
+from savant_rs.utils import eval_expr
+from savant_rs.match_query import (
+    TlsConfig,
+    register_etcd_resolver,
+    register_env_resolver,
+    EtcdCredentials,
+)
+from pathlib import Path
+
+register_env_resolver()
+
+# if not set env var RUN_ETCD_TESTS=1, skip
+if not eval_expr('env("RUN_ETCD_TESTS", 0)') == 0:
+    print("Skipping etcd tests")
+    exit(0)
+
+# read ca from file to string
+ca = Path("../../etcd_dynamic_state/assets/certs/ca.crt").read_text()
+cert = Path("../../etcd_dynamic_state/assets/certs/client.crt").read_text()
+key = Path("../../etcd_dynamic_state/assets/certs/client.key").read_text()
+
+conf = TlsConfig(
+    ca,
+    cert,
+    key,
+)
+
+creds = EtcdCredentials("root", "qwerty")
+
+register_etcd_resolver(
+    hosts=["https://127.0.0.1:2379"], credentials=creds, tls_config=conf, watch_path=""
+)
+
+print(eval_expr('etcd("foo/bar", "default")'))
