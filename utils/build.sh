32b72444982eb3f66881e914f54728c92cf4d0ab
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-01-09 18:28:59 +0100

    fixed multi-python builder

--- utils/build.sh
+++ utils/build.sh
@@ -13,13 +13,23 @@ export LD_LIBRARY_PATH=$RUST_STD_DIR:$(pwd)/target/$MODE:$(pwd)/target/$MODE/dep
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CARGO_TARGET_DIR/$MODE:$CARGO_TARGET_DIR/$MODE/deps
 echo $LD_LIBRARY_PATH
 
+MATURIN_PYTHON_SEARCH_ARGS=-f
+
+if [[ -z $PYTHON_INTERPRETER ]]; then
+    MATURIN_PYTHON_SEARCH_ARGS=-f
+else
+    MATURIN_PYTHON_SEARCH_ARGS="-i $PYTHON_INTERPRETER"
+fi
+
 cd $PROJECT_DIR/savant_python
 if [ "$MODE" = "debug" ]; then
-  EXTRA_FLAGS=""
+  EXTRA_FLAGS="$MATURIN_PYTHON_SEARCH_ARGS"
 else
-  EXTRA_FLAGS="--release"
+  EXTRA_FLAGS="--release $MATURIN_PYTHON_SEARCH_ARGS"
 fi
+
 CARGO_INCREMENTAL=true maturin build $EXTRA_FLAGS -o $PROJECT_DIR/dist
+
 cd $PROJECT_DIR
 
 for d in $(find savant_plugins/* -maxdepth 0 -type d); do
