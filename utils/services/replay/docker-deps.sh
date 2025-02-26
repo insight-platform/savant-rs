757a081de12236005d5265a9e565aa2c6cd4375d
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-02-26 12:48:00 +0100

    fix for tag env variable configuration

--- utils/services/replay/docker-deps.sh
+++ utils/services/replay/docker-deps.sh
@@ -1,22 +1,23 @@
 #!/bin/sh -e
 
-apt-get update && apt install -y clang \
-  libgstreamer1.0-dev \
-  libgstreamer-plugins-base1.0-dev \
-  libgstreamer-plugins-bad1.0-dev \
-  gstreamer1.0-plugins-base \
-  gstreamer1.0-plugins-good \
-  gstreamer1.0-plugins-bad \
-  gstreamer1.0-plugins-ugly \
-  gstreamer1.0-libav \
-  gstreamer1.0-tools \
-  gstreamer1.0-x \
-  gstreamer1.0-alsa \
-  gstreamer1.0-gl \
-  gstreamer1.0-gtk3 \
-  gstreamer1.0-qt5 \
-  gstreamer1.0-pulseaudio \
-  libpython3-dev
+apt-get update && apt install -y clang
+#\
+#  libgstreamer1.0-dev \
+#  libgstreamer-plugins-base1.0-dev \
+#  libgstreamer-plugins-bad1.0-dev \
+#  gstreamer1.0-plugins-base \
+#  gstreamer1.0-plugins-good \
+#  gstreamer1.0-plugins-bad \
+#  gstreamer1.0-plugins-ugly \
+#  gstreamer1.0-libav \
+#  gstreamer1.0-tools \
+#  gstreamer1.0-x \
+#  gstreamer1.0-alsa \
+#  gstreamer1.0-gl \
+#  gstreamer1.0-gtk3 \
+#  gstreamer1.0-qt5 \
+#  gstreamer1.0-pulseaudio \
+#  libpython3-dev
 
 ARCH=$(uname -m)
 
