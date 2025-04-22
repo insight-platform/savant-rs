07df5761d462c3b47d9f428fe8a935041df0e7f9
Ivan Kud <bwsw@users.noreply.github.com>
2025-04-22 14:06:16 +0200

    Retina rtsp (#181)
    Implemented experimental multistream RTSP adapter based on Retina RTSP client.

---
+++ services/retina_rtsp/src/configuration.rs
@@ -0,0 +1,83 @@
+use anyhow::Result;
+use hashbrown::{HashMap, HashSet};
+use replaydb::job_writer::SinkConfiguration;
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
+use twelf::{config, Layer};
+
+const DEFAULT_RECONNECT_INTERVAL: Duration = Duration::from_secs(5);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RtspSourceOptions {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RtspSource {
+    pub source_id: String,
+    pub url: String,
+    pub stream_position: Option<usize>,
+    pub options: Option<RtspSourceOptions>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct SyncWindowDurationConfiguration {
+    pub group_window_duration: Duration,
+    pub batch_duration: Duration,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RtspSourceGroup {
+    pub sources: Vec<RtspSource>,
+    pub rtcp_sync: Option<SyncWindowDurationConfiguration>,
+}
+
+#[config]
+#[derive(Debug, Serialize, Clone)]
+pub struct ServiceConfiguration {
+    pub sink: SinkConfiguration,
+    pub rtsp_sources: HashMap<String, RtspSourceGroup>,
+    pub reconnect_interval: Option<Duration>,
+}
+
+impl ServiceConfiguration {
+    pub fn new(path: &str) -> Result<Self> {
+        let conf = {
+            let mut conf = Self::with_layers(&[Layer::Json(path.into())])?;
+            if conf.reconnect_interval.is_none() {
+                conf.reconnect_interval = Some(DEFAULT_RECONNECT_INTERVAL);
+            }
+            conf
+        };
+
+        {
+            // check uniqueness of group_name
+            let group_names = conf.rtsp_sources.keys().collect::<HashSet<_>>();
+            if group_names.len() != conf.rtsp_sources.len() {
+                return Err(anyhow::anyhow!(
+                    "group names must be unique among the groups."
+                ));
+            }
+
+            // all sources must have unique source_id
+            let mut all_source_ids: Vec<String> = Vec::new();
+            for group in conf.rtsp_sources.values() {
+                let source_ids = group
+                    .sources
+                    .iter()
+                    .map(|s| s.source_id.clone())
+                    .collect::<Vec<_>>();
+                all_source_ids.extend_from_slice(&source_ids);
+            }
+            let unique_source_ids = all_source_ids.iter().collect::<HashSet<_>>();
+            if unique_source_ids.len() != all_source_ids.len() {
+                return Err(anyhow::anyhow!(
+                    "The source_id field must be unique among the sources."
+                ));
+            }
+        }
+
+        Ok(conf)
+    }
+}
---
+++ services/retina_rtsp/src/main.rs
@@ -0,0 +1,68 @@
+mod configuration;
+mod service;
+
+use crate::configuration::ServiceConfiguration;
+use anyhow::{anyhow, Result};
+use log::{debug, error, info};
+use replaydb::job_writer::JobWriter;
+use retina::client::SessionGroup;
+use savant_core::transport::zeromq::NonBlockingWriter;
+use service::run_group;
+use std::{env::args, sync::Arc};
+use tokio::{sync::Mutex, task::JoinSet};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("┌───────────────────────────────────────────────────────┐");
+    println!("│                  Retina RTSP Service                  │");
+    println!("│ This program is licensed under the APACHE 2.0 license │");
+    println!("│      For more information, see the LICENSE file       │");
+    println!("│            (c) 2025 BwSoft Management, LLC            │");
+    println!("└───────────────────────────────────────────────────────┘");
+
+    env_logger::init();
+    let conf_arg = args()
+        .nth(1)
+        .ok_or_else(|| anyhow!("missing configuration argument"))?;
+    info!("Configuration: {}", conf_arg);
+    let conf = Arc::new(ServiceConfiguration::new(&conf_arg)?);
+    debug!("Configuration: {:?}", conf);
+
+    let rtsp_session_group = Arc::new(SessionGroup::default());
+
+    let non_blocking_writer = NonBlockingWriter::try_from(&conf.sink)?;
+    let sink = Arc::new(Mutex::new(JobWriter::new(non_blocking_writer)));
+
+    let mut jobs = JoinSet::new();
+    for (group_name, _) in &conf.rtsp_sources {
+        let conf = conf.clone();
+        // check uniqueness of group_name
+        let group_name = group_name.clone();
+        let rtsp_session_group = rtsp_session_group.clone();
+        let sink = sink.clone();
+        jobs.spawn(async move {
+            loop {
+                tokio::time::sleep(conf.reconnect_interval.unwrap()).await;
+                let service_group_res = run_group(
+                    conf.clone(),
+                    group_name.clone(),
+                    rtsp_session_group.clone(),
+                    sink.clone(),
+                )
+                .await;
+                if let Err(e) = service_group_res {
+                    error!("Error running service group {}: {:?}", group_name, e);
+                    continue;
+                }
+                info!("Service group {} stopped", group_name);
+            }
+        });
+    }
+
+    tokio::signal::ctrl_c().await?;
+    rtsp_session_group.await_teardown().await?;
+    jobs.abort_all();
+
+    info!("Press Ctrl+C to stop the service");
+    Ok(())
+}
