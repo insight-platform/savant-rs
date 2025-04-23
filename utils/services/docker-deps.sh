33679778da468c4751cbccb5b1b0a02e7e03d48b
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-04-23 16:08:48 +0200

    fixed build

--- utils/services/docker-deps.sh
+++ utils/services/docker-deps.sh
@@ -6,6 +6,7 @@ apt-get update && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     clang \
     ca-certificates \
+    libva-dev \
     curl \
     unzip && \
     apt-get clean && \
